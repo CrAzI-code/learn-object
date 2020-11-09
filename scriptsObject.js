88b3c304c5b6eb335b7003eb56964ad25cbb2efd
Crazi code <emmaobiora96@gmail.com>
2020-11-09 13:06:19 +0000

    javascript object

---
+++ scriptsObject.js
@@ -0,0 +1,15 @@
+const {log} = console;
+
+
+let listOfNumbers = [2, 3, 5, 7, 11];
+log(listOfNumbers[2]);
+
+log(listOfNumbers[2-1]);
+log(listOfNumbers["length"]);
+log(listOfNumbers.length);
+
+let doh = "Doh";
+console.log(typeof doh.toUpperCase)
+
+let anObject = {left: 1, right: 2};
+ log("left" in anObject)
